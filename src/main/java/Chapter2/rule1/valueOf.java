c6c2ae18bb5b1d5d0ca67bcee511f3212d35d40c
lixiaofeng <alstonxf@126.com>
2021-12-24 12:45:48 +0800

    intstream

---
+++ src/main/java/Chapter2/rule1/valueOf.java
@@ -0,0 +1,7 @@
+package Chapter2.rule1;
+
+public class valueOf {
+    public static Boolean valueOf(Boolean b){
+        return b ? Boolean.TRUE:Boolean.FALSE;
+    }
+}
---
+++ src/main/java/test/test1.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test1 {
+    public static void main(String[] args) {
+        String t = "1234567";
+        System.out.println(t.substring(1,4));
+    }
+}
