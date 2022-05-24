9ccbe16df0c440f82b7f2d9596f2a7aa765ff298
李晓峰 <alstonxf@126.com>
2022-05-24 21:31:21 +0800

    '添加167题解答'

--- src/main/java/LeetCode/TwoPointers/Solution167.java
+++ src/main/java/LeetCode/TwoPointers/Solution167.java
@@ -1,14 +1,13 @@
 package LeetCode.TwoPointers;
+
+import java.util.Arrays;
+
 /*
 167. 两数之和 II - 输入有序数组
         给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
-
         以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
-
         你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
-
         你所设计的解决方案必须只使用常量级的额外空间。
-
 
         示例 1：
 
@@ -37,12 +36,30 @@ package LeetCode.TwoPointers;
 */
 public class Solution167 {
     public int[] twoSum(int[] numbers, int target) {
-        int[] result = new int[0];
+        int[] result = new int[2];
 
+        for (int i = 0;i<numbers.length;i++){
+            for(int j =i+1;j<numbers.length;j++){
+                int s = numbers[i]+numbers[j];
+                if (s==target){
+                    result[0] = i+1;
+                    result[1] = j+1;
+                    return result;
+                }
+                if(s>target){
+                    break;
+                }
+            }
+        }
         return result;
     }
 
     public static void main(String[] args) {
-
+//        int[] numbers = new int[]{2,7,11,15};
+//        int target = 9;
+        int[] numbers = new int[]{1,2,3,4,4,9,56,90};
+        int target = 8;
+        int[] result = new Solution167().twoSum(numbers,target);
+        System.out.println(Arrays.toString(result));
     }
 }
