23071e873bfce4983ac3d1d8b79e24513f1bfe74
‘lixiaofeng’ <alstonxf@126.com>
2022-07-11 13:34:02 +0800

    新增SpringDemo文档

--- src/main/java/SpringDemo/App.java
+++ src/main/java/SpringDemo/App.java
@@ -3,6 +3,12 @@ package SpringDemo;
 import javafx.application.Application;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Spring Core:核心容器提供Spring框架的基本功能，其主要组件BeanFactory是工厂模式的实现。它通过控制反转（IOC）机制，将应用程序配置和依赖性规范与实际的程序代码分离开。
+ * Spring Context:想Spring框架提供上下文信息，包括企业服务。
+ * Spring DAO：JDBC DAO抽象层提供了有用的一场层次结构，用来管理异常处理和不同数据库供应商抛出的错误信息。
+ */
 public class App {
     public static void main( String[] args ) {
         //获取ApplicationContext对象
--- src/main/java/SpringDemo/Test.java
+++ src/main/java/SpringDemo/Test.java
@@ -1,6 +1,9 @@
 package SpringDemo;
 
 public class Test
+        /**
+         * 当创建一个简单的Java项目的时候，在控制台要求输出“Hello world!”,如果要求输出“Hello Beer!”等等，那么就要不断地去修改程序源代码。如果项目庞大，程序中涉及这一输出地语句不止一处，有很多处，且分散在整个项目地源码（往往有成千上万）中，如此大动干戈地改动程序，对于一个软件系统地维护来说将是灾难性地。
+         */
 {
     public static void main( String[] args )
     {
