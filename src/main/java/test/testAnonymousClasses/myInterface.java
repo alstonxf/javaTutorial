d69f40af42c673e296dc1e0c133e882a657bcb92
‘lixiaofeng’ <alstonxf@126.com>
2023-02-03 20:58:49 +0800

    测试匿名内部类

---
+++ src/main/java/test/testAnonymousClasses/myInterface.java
@@ -0,0 +1,5 @@
+package com.flink.tutorials.java.myProject.DataLibrary.test.testAnonymousClasses;
+
+public interface myInterface {
+    public abstract void myMethod();
+}
---
+++ src/main/java/test/testAnonymousClasses/testAnonymousClasses.java
@@ -0,0 +1,27 @@
+package com.flink.tutorials.java.myProject.DataLibrary.test.testAnonymousClasses;
+
+
+public class testAnonymousClasses {
+    public static void main(String[] args) {
+        //使用一般类实现接口
+        class myclass implements myInterface{
+
+            @Override
+            public void myMethod() {
+                System.out.println("使用一般类实现接口...");
+            }
+        };
+        //有类名
+        new myclass().myMethod();
+
+        //使用匿名内部类直接实现接口
+        myInterface myInterface1 = new myInterface() {
+            @Override
+            public void myMethod() {
+                System.out.println("使用匿名内部类直接实现接口....");
+            }
+        };
+        //没有类名，只有接口名
+        myInterface1.myMethod();
+    }
+}
